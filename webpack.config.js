ae9a3aa4a0fb4b111ecdc55a6b9d5a188eac745f
saxum2010 <saxum2010@gmail.com>
2017-11-28 14:24:54 +0200

    lesson12: vue init webpack-simple

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,12 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './app.js',
+  entry: './src/main.js',
   output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
     filename: 'build.js'
   },
-  watch: true,
-   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
-  ],
   module: {
     rules: [
       {

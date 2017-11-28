5b47bd3157dce84c89c764a972a8cd8181cdc9ec
saxum2010 <saxum2010@gmail.com>
2017-11-28 15:17:42 +0200

    lesson13: vue-resource

--- src/main.js
+++ src/main.js
@@ -1,7 +1,61 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
+
+Vue.use(VueResource)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  	data: {
+        endpoint: 'https://jsonplaceholder.typicode.com/posts',
+        posts: [],
+        post: {}
+    },
+ 	computed: {
+        resource: function() {
+            return this.$resource('https://jsonplaceholder.typicode.com/posts{/id}')
+        }
+    },
+	methods: {
+		savePost: function() {
+
+            this.resource.save(this.post)
+
+        },
+        getSinglePost: function(post_id) {
+
+            this.resource.get({ id: post_id }).then(function(response) {
+
+                this.post = response.data
+
+            })
+
+        },
+		 getAllPosts: function() {
+
+            var options = {
+                params: {
+                    _start: 10,
+                    _limit: 5
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+
+            // this.$http.get(this.endpoint, options).then(function(response) {
+            this.resource.get().then(function(response) {
+                this.posts = response.data
+
+            }, function(error) {
+                // ошибка
+            })
+
+        }
+	},
+    created: function() {
+        this.getSinglePost(100)
+        this.getAllPosts()
+    }
+  // render: h => h(App)
 })
